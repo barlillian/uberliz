704bc38061ded28c83745e8bbd1049e76e520ea1
liz chen <lizchen@lizde-MBP.home>
2025-09-24 11:54:03 +0200

    Initial MVP commit

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,14 @@
-// backend/config.js
 require("dotenv").config();
+
+const ENV = process.env.NODE_ENV || "local";
 
 module.exports = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
-  REDIRECT_URI: process.env.REDIRECT_URI,
+  REDIRECT_URI:
+    ENV === "prod"
+      ? process.env.REDIRECT_URI_PROD
+      : process.env.REDIRECT_URI_LOCAL,
   UBER_API_BASE_URL: process.env.UBER_API_BASE_URL,
   PORT: process.env.PORT || 3000,
 };
